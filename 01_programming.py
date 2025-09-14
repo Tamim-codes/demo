f2ed14e8f4e35f9a72adb1f977f1d89bfa58143d
Tamim <ahad@2009.in>
2025-09-14 10:52:43 +0600

    dfa

---
+++ 01_programming.py
@@ -0,0 +1,64 @@
+# print("Hello World")
+# nt("TAMIM","AHMED","AHAD",34)
+# print(3>2)a = "Hellon World"
+# print(type(a))
+# print("12 + 32")
+# pri
+
+
+
+
+
+# import turtle
+# import colorsys
+# t = turtle.Turtle()
+# s = turtle.Screen().bgcolor('black')
+# t.speed(0)
+# n = 70
+# h = 0
+# for i in range(360):
+#     c = colorsys.hsv_to_rgb(h, 1, 0.8)
+#     h+= 1/n
+#     t.color(c)
+#     t.left(1)
+#     t.fd(1)
+#     for j in range(2):
+#         t.left(2)
+#         t.circle(100)
+
+
+
+
+# import  math
+# from turtle import *
+# def hearta(k) :
+#        return 15*math.sin(k)**3
+# def heartb(k) :
+#        return 12*math.cos(k)-5*\
+#                 math.cos(2*k)-2*\
+#                 math.cos(3*k)-\
+#                 math.cos(4*k)
+# speed(10000000)
+# bgcolor("black")
+# for i in range(6000):
+#     goto(hearta(i)*20,heartb(i)*20)
+#     for j in range(5):
+#         color('#f73487')
+#     goto(0,0)
+# done()
+
+
+
+
+# import turtle
+# turtle.bgcolor("black")
+
+# squary = turtle.Turtle()
+# squary.speed(20)
+# squary.pencolor("red")
+# for i in range(400):
+#     squary.forward(i)
+#     squary.left(91)
+
+
+
